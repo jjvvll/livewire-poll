ca7655ba15ad5143a6e97b8083efe763a6b2dbff
jjvvll <llanetajohnver@gmail.com>
2025-02-13 14:22:28 +0800

    Daily updates

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,9 +7,27 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [''];
 
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption(){
+        $this->options[] ='';
+    }
+
+    public function removeOption($index){
+        unset($this->options[$index]); //remove element
+        $this->options = array_values($this->options) ; //make array continuous again
+    }
+
+    public function mount(){
+        //can be used to initialize the values of properties , will only be called once
+        //for example reading something from database, you do that here
+    }
 }
+
+
+//this is called a component
